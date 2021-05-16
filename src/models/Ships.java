99da184bb37617dd4d9c60d6b66168e92aa48572
Arlamovskiy-Ruslan <55766007+Arlamovskiy-Ruslan@users.noreply.github.com>
2021-05-16 22:10:41 +0300

    ships

--- src/models/Ships.java
+++ src/models/Ships.java
@@ -1,2 +1,20 @@
-package models;public class Ships {
+package models;
+
+import io.IPrint;
+
+import java.util.Scanner;
+
+public class Ships {
+
+    public void placeShips(Scanner scanner, IPrint printer){
+        printer.print("Please enter OX coordinate:");
+        int x = scanner.nextInt();
+        printer.print("Please enter OY coordinate:");
+        int y = scanner.nextInt();
+
+        printer.print("Choose direction:");
+        printer.print("1. Vertical:");
+        printer.print("2. Horizontal:");
+        int rotation = scanner.nextInt();
+    }
 }
