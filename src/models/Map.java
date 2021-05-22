394f2c526c3095a1bf5aebd2b1f10a438adbd516
Arlamovskiy-Ruslan <55766007+Arlamovskiy-Ruslan@users.noreply.github.com>
2021-05-22 23:04:51 +0300

    map

--- src/models/Map.java
+++ src/models/Map.java
@@ -10,9 +10,9 @@ public class Map {
         this.print = print;
     }
 
-    public void map(IPrint printer){
+    public void map(IPrint printer) {
         printer.print("  0 1 2 3 4 5 6 7 8 9");
         for (int counter = 0; counter < 10; counter++)
-        printer.print(counter + " - - - - - - - - - -");
+            printer.print(counter + " - - - - - - - - - -");
     }
 }
