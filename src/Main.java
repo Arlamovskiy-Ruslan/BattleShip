862896d939bcdafd3e6126101e31972763607295
Arlamovskiy-Ruslan <55766007+Arlamovskiy-Ruslan@users.noreply.github.com>
2021-05-22 23:03:39 +0300

    mainProgram

--- src/Main.java
+++ src/Main.java
@@ -3,21 +3,24 @@ import io.IPrint;
 import io.Print;
 import models.Map;
 import models.Players;
+import models.Ships;
 
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args){
-        Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
         IPrint printer = new Print();
 
         Players players = new Players();
         Map map = new Map(printer);
+        Ships ships = new Ships();
 
         players.inputPlayersName(scanner);
         players.outputPlayersName();
         map.map(printer);
+        ships.chooseCoordinate(scanner, printer);
 
     }
 }
